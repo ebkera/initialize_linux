4f01cce75d0bc3b295b907fe198816a0191153bf
Eranjan Kandegedara <36378725+ebkera@users.noreply.github.com>
2020-10-22 23:16:07 -0500

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+
+## General stuff here
 sudo apt update
 sudo apt upgrade
 
+## If this is a laptop and the OS is ubuntu then ususlly overheating occurs so we need TLP
+
+# This is from: https://itsfoss.com/reduce-overheating-laptops-linux/
+sudo add-apt-repository ppa:linrunner/tlp
+sudo apt-get update
+sudo apt-get install tlp tlp-rdw
+# If you are using ThinkPads, you require an additional step:
+# sudo apt-get install tp-smapi-dkms acpi-call-dkms
+#To uninstall TLP, you can use the following commands:
+#sudo apt-get remove tlp
+#sudo add-apt-repository --remove ppa:linrunner/tlp
