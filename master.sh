29c53ad66f8c2749e762dc6f603646a050839af6
Eranjan Kandegedara <36378725+ebkera@users.noreply.github.com>
2020-11-03 20:19:59 -0600

    added curl and MS Teams

--- master.sh
+++ master.sh
@@ -4,9 +4,10 @@
 ## General stuff here
 sudo apt update
 sudo apt upgrade
+# Adding commonly required packages
+sudo apt install curl
 
-## If this is a laptop and the OS is ubuntu then ususlly overheating occurs so we need TLP
-
+## If this is a laptop and the OS is ubuntu then usually overheating occurs so we need TLP
 # This is from: https://itsfoss.com/reduce-overheating-laptops-linux/
 sudo add-apt-repository ppa:linrunner/tlp
 sudo apt-get update
@@ -30,6 +31,15 @@ git config --global user.email johndoe@hotmail.com
 
 # pulling my own repos
 path_to_packages=$(python3 -m site --user-site)
-echo "Path to packages: $path_to_packages"
+echo "Path to python packages: $path_to_packages"
+echo "git cloning my repos..."
 cd $path_to_packages
 git clone https://github.com/ebkera/ebk.git
+
+# Installing MS-teams (not in repos)
+echo "Installing MS-Teams"
+echo "If installation fails make sure you have curl installed... try sudo apt install curl"
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
+sudo apt update
+sudo apt install teams
