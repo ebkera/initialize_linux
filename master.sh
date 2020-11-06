080f12e48bcdaf297a9fc1d9be8867040c0178df
Eranjan Kandegedara <36378725+ebkera@users.noreply.github.com>
2020-11-05 20:03:47 -0600

    working

--- master.sh
+++ master.sh
@@ -42,7 +42,6 @@ git clone https://github.com/ebkera/ebk.git
 # carbon ssh config file and settings
 wget -O ~/.ssh/config https://raw.githubusercontent.com/ebkera/initialize_linux/main/config_ssh_carbon
 read -p "Enter user name for ANL: " varname
-echo 
 sed -i "s|uname|${varname}|g" ~/.ssh/config
 
 # Installing MS-teams (not in repos)
